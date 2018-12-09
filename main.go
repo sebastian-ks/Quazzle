518c2873b73710d4eb131588c904b4a49da8fc6a
Philipp Merz <phmrch@gmail.com>
2018-12-09 13:07:34 +0100

    Added config file support

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Port string `json:"port"`
+}
+
 func main() {
+	conf := getConfig()
+
 	//Static files like css and js
 	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
 	http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("script"))))
@@ -14,23 +24,45 @@ func main() {
 	http.HandleFunc("/login", handleLogin)
 	http.HandleFunc("/register", handleRegister)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(conf.Port, nil)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	templ, err := template.ParseFiles("index.html")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	templ.Execute(w, nil)
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
 	templ, err := template.ParseFiles("login.html")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	templ.Execute(w, nil)
 }
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
 	templ, err := template.ParseFiles("register.html")
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	templ.Execute(w, nil)
+}
+
+func getConfig() Config {
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	defer jsonFile.Close()
+	data, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var conf Config
+	json.Unmarshal([]byte(data), &conf)
+	return conf
 }
