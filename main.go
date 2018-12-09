8507da1f4a2e0df01011b9e48925da5a19550881
Philipp Merz <phmrch@gmail.com>
2018-12-09 19:29:33 +0100

    Started login feature

--- main.go
+++ main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type config struct {
-	Port string `json:"port"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 type userType struct {
-	Name string
+	Name     string
+	Id       int
+	Seshid   int
+	Password string
 }
 
 var user userType
 
+var db *sql.DB
+
 func main() {
 	conf := getConfig()
+
+	db = initDBConn(conf)
+	defer db.Close()
 
 	//Static files like css and js
 	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
@@ -53,7 +67,17 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 		templ.Execute(w, nil)
 	} else if r.Method == "POST" {
 		r.ParseForm()
-		user.Name = r.Form["username"][0]
+
+		rows, err := db.Query("SELECT hashed_pw FROM users WHERE name=\"" + r.Form["username"][0] + "\"")
+		checkErr(err)
+		rows.Next()
+		rows.Scan(&user.Password)
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(r.Form["password"][0])); err != nil {
+			fmt.Println(err, "Validation unsuccesfull")
+		} else {
+			fmt.Println("Validation succesfull")
+		}
+
 	}
 
 }
@@ -64,6 +88,15 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	templ.Execute(w, nil)
+}
+
+func initDBConn(conf config) *sql.DB {
+	database, err := sql.Open("mysql", conf.Username+":"+conf.Password+"@tcp(localhost:3306)/quazzle")
+	checkErr(err)
+
+	err = database.Ping()
+	checkErr(err)
+	return database
 }
 
 func getConfig() config {
@@ -80,4 +113,17 @@ func getConfig() config {
 	var conf config
 	json.Unmarshal([]byte(data), &conf)
 	return conf
+}
+
+func hashPW(pw string) []byte {
+	fmt.Println([]byte(pw))
+	hash, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.MinCost)
+	checkErr(err)
+	return hash
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
